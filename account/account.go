1435c54770cb0c55029c033f0818365c84b24765
0xSherlokMo <mohamed.sorour@halan.com>
2023-10-24 17:23:42 +0300

    declear accounts model

--- account/account.go
+++ account/account.go
@@ -1,26 +1,15 @@
 package account
 
 import (
-	"sync"
-
 	"github.com/google/uuid"
 )
 
 type Account struct {
-	ID      uuid.UUID  `json:"id"`
-	Name    string     `json:"name"`
-	Balance float64    `json:"balance"`
-	latch   sync.Mutex `json:"-"`
+	ID      uuid.UUID `json:"id"`
+	Name    string    `json:"name"`
+	Balance float64   `json:"balance,string"`
 }
 
 func (a *Account) GetID() string {
 	return a.ID.String()
-}
-
-func (a *Account) Lock() {
-	a.latch.Lock()
-}
-
-func (a *Account) Unlock() {
-	a.latch.Unlock()
 }
