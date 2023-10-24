8cba425ddbb6ecd738c04b7fd68f0cadc758c3bf
0xSherlokMo <mohamed.sorour@halan.com>
2023-10-24 17:23:30 +0300

    feaT: add set and setnx

--- memorydb/memory.go
+++ memorydb/memory.go
@@ -5,11 +5,16 @@ import (
 	"sync"
 )
 
+type Key = string
+
 type IdentifiedRecord interface {
 	GetID() string
 }
 
-var ErrRowLocked = errors.New("row_is_locked")
+var (
+	ErrRowLocked    = errors.New("row_is_locked")
+	ErrRecordExists = errors.New("record_exists")
+)
 
 type document[object IdentifiedRecord] struct {
 	mu   sync.Mutex
@@ -17,31 +22,49 @@ type document[object IdentifiedRecord] struct {
 }
 
 type MemoryDB[T IdentifiedRecord] struct {
-	records map[string]*document[T]
+	records map[Key]*document[T]
 }
 
 func Default[T IdentifiedRecord]() *MemoryDB[T] {
 	return &MemoryDB[T]{
-		records: make(map[string]*document[T]),
+		records: make(map[Key]*document[T]),
 	}
 }
 
-func (m *MemoryDB[T]) Set(key string, record T) error {
-	row, exists := m.records[key]
+func (m *MemoryDB[T]) Setnx(key Key, record T) error {
+	_, exists := m.records[key]
 	if exists {
-		row.mu.Lock()
-		defer row.mu.Unlock()
+		return ErrRecordExists
 	}
+
 	m.records[key] = &document[T]{
 		data: record,
 	}
 	return nil
 }
 
-func (m *MemoryDB[T]) Keys() []string {
-	var keys []string
+func (m *MemoryDB[T]) Set(key Key, record T) {
+	row, exists := m.records[key]
+	if !exists {
+		m.records[key] = &document[T]{
+			data: record,
+		}
+		return
+	}
+
+	row.mu.Lock()
+	defer row.mu.Unlock()
+	row.data = record
+}
+
+func (m *MemoryDB[T]) Keys() []Key {
+	var keys []Key
 	for k := range m.records {
 		keys = append(keys, k)
 	}
 	return keys
+}
+
+func (m *MemoryDB[T]) Length() int {
+	return len(m.records)
 }
